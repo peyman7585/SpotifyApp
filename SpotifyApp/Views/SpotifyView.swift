127fb37951d6c1b702ada92696237181f34d6e9c
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-03-26 00:18:13 +0330

    design  main ui

--- SpotifyApp/Views/SpotifyView.swift
+++ SpotifyApp/Views/SpotifyView.swift
@@ -9,14 +9,58 @@ import SwiftUI
 
 struct SpotifyView: View {
     var body: some View {
-        ZStack{
-            
-            Image("billi-pic")
-                .resizable()
-            
-            
-                .edgesIgnoringSafeArea(.all)
-        }
+        NavigationView{
+            ZStack{
+                Image("billi-pic")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack{
+                    Image("spotify")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 196, height: 59)
+                    Spacer()
+                    
+                }
+                VStack{
+                    
+                    Spacer()
+                    VStack{
+                        Text("Enjoy Listening To Music")
+                            .font(.title2)
+                            .foregroundColor(.white)
+                            .padding()
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.")
+                            .frame(width: 297)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .padding(.bottom,25)
+                            
+                    }
+                    VStack{
+                        Button{
+                            
+                        }label: {
+                            
+                                Text("Get Started")
+                                    .font(.title2)
+                                    .frame(width: 329, height: 92)
+                                    .foregroundColor(.white)
+                                    .background(Color("ButtonColor"))
+                                    .cornerRadius(25)
+                                
+                        }
+                      
+                    }
+                
+                    .padding(.bottom,40)
+                }
+
+            }
+                
+            }
+
     }
 }
 
